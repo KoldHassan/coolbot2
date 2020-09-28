1fc46b5474f6fe8ba9daee7fb0836c8042fed5ae
hassan0315 <70412930+hassan0315@users.noreply.github.com>
2020-09-28 12:20:18 -0500

    Add files via upload

---
+++ makedictionaries.py
@@ -0,0 +1,10 @@
+def make_dictionaries():
+  wisdom = {
+        "Kauffman": [
+            "I have NEVER taken drugs",
+            "nivel INFERIOR",
+            "más detalles",
+            "You should switch to HL",
+         ],
+ }
+  return wisdom
